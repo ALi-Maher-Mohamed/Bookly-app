6bb8744c6bdc51158dadf748953781cc2494c434
alimaher <ali.maher0013@gmail>
2024-10-11 21:08:09 +0300

    creation featured books method

--- lib/feature/home/presentation/manager/feature_books_cubit/featured_books_cubit.dart
+++ lib/feature/home/presentation/manager/feature_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,27 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/feature/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/feature/home/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedCubit extends Cubit<FeaturedState> {
-  FeaturedCubit() : super(FeaturedInitial());
+  FeaturedCubit(this.homeRepo) : super(FeaturedInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedLoading());
+    var restult = await homeRepo.fetchFeaturedBooks();
+
+    restult.fold((failure) {
+      emit(
+        FeaturedFailure(failure.errMessage),
+      );
+    }, (books) {
+      emit(
+        FeaturedSuccess(books),
+      );
+    });
+  }
 }
